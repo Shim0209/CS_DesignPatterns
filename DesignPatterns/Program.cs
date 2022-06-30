ce840e58d0db965ee9ce194588478473225f1e4f
yeonseong shim <shimyseong@gmail.com>
2022-07-01 04:56:53 +0900

    Add 디자인패턴 프로젝트

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * 디자인패컨 카테고리
+ * - 생성 패턴 (Creational Patterns) : 특정 클래스의 객체를 생성하는데 사용되는 패턴들
+ * - 구조 패턴 (Structural Patterns) : 여러 클래스 혹은 객체들이 어떻게 결합하여 하나의 큰 구조를 만드는지에 대한 패턴들
+ * - 행위 패턴 (Behavioral Patterns) : 여러 객체들 간에 어떻게 상호작용하고 통신하는지에 포커스를 맞춘 패턴들
+ */
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
